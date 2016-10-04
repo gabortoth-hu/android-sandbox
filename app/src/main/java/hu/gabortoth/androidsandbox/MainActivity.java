aa7343951a55309e0d1bf43f76323bffa1cfcefd
Gabor Toth <gabor.toth@live.com>
2016-10-04 20:23:43 +0200

    Show 'DisplayMessageActivity', forward data using Intent

--- app/src/main/java/hu/gabortoth/androidsandbox/MainActivity.java
+++ app/src/main/java/hu/gabortoth/androidsandbox/MainActivity.java
@@ -1,13 +1,33 @@
 package hu.gabortoth.androidsandbox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // In order for the system to match this method to
+    // the method name given to android:onClick, the
+    // method must:
+    //   o be public
+    //   o have a void return value
+    //   o have a View as the only parameter
+    //     (this will be the View that was clicked)
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
