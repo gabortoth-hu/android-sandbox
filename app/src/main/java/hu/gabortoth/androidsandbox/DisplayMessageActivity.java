fc4889fdff16c3cd716a9460c3b819cedb790ac7
Gabor Toth <gabor.toth@live.com>
2016-10-19 08:54:46 +0200

    Add activity lifecycle logs to DisplayMessageActivity

--- app/src/main/java/hu/gabortoth/androidsandbox/DisplayMessageActivity.java
+++ app/src/main/java/hu/gabortoth/androidsandbox/DisplayMessageActivity.java
@@ -4,10 +4,48 @@ import android.content.Intent;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private static final String TAG = "DisplayMessageActivity";
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d(TAG, "onRestart");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
